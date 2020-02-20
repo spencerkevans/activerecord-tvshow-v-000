c776bdb92b59c513d6bee8a96e30b6f6b0dd4eba
spensation <spencevans@gmail.com>
2020-02-20 17:01:22 -0500

    table migrated

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,16 @@
+class CreateShows < ActiveRecord::Migration[5.2]
+  def up
+  end
+ 
+  def down
+  end
+
+  def change
+  	create_table :shows do |t|
+  		t.string :name
+  		t.string :day
+  		t.string :network
+  		t.integer :rating
+  	end
+  end
+end
